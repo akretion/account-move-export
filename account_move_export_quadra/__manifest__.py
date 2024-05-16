f1badb8b182d2bbd0dc4aab382a154ae1a4a6579
hparfr <raphael.reverdy@akretion.com>
2024-05-16 16:42:44 +0200

    account_move_export_quadra: add module

---
+++ account_move_export_quadra/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Akretion France (http://www.akretion.com/)
+# @author: Raphaël Reverdy <raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Move Export Quadra",
+    "version": "16.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "summary": "Export journal entries to Quadratus Compta",
+    "author": "Akretion",
+    "maintainers": ["hparfr"],
+    "website": "https://github.com/akretion/account-move-export",
+    "depends": ["account_move_export"],
+    "installable": True,
+    "data": [
+        "data/account_move_export_quadra_config.xml",
+    ],
+}

3ac7cec83d325b6b5c920a7d447a215ad8bae755
TB-Ph35 <thomas.bonneru@phelumtech.fr>
2025-08-30 22:26:08 +0200

    fixup! Fix analytic amount

--- account_move_export_quadra/models/account_analytique_line.py
+++ account_move_export_quadra/models/account_analytique_line.py
@@ -10,7 +10,7 @@ class AccuntAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
     def _prepare_account_move_export_line(self, export_options):
-        if "attachemnts" in export_options.keys():
+        if "cols_analytic" in export_options.keys():
             amount = str(abs(int(self.amount * 100)))
 
             code = str(self.x_plan2_id.code)
